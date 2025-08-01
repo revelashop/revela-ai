5ce5b5829a662e9e94e091081de6b6c981b5e6aa
revelashop <revela.shop2025@gmail.com>
2025-08-01 16:23:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -39,4 +40,5 @@ def ask():
     return render_template("index.html", response=reply)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
